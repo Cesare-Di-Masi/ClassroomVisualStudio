ce01d7423438a13f2226fdd58b5acac6caaeba9f
cesare di masi <cesare.dimasi.stud@ispascalcomandini.it>
2025-03-15 09:47:25 +0100

    matrix exercise

---
+++ MauiApp1/WpfApp1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit;
+using CommunityToolkit.Mvvm.ComponentModel;
+using WpfApp1.Models;
+
+namespace WpfApp1.ViewModels
+{
+    public partial class MainWindowViewModel:ObservableObject
+    {
+        [ObservableProperty]
+        private string name;
+        [ObservableProperty]
+        private bool isMale;
+
+        public ObservableCollection<Animal> Animals;
+
+
+    }
+}
