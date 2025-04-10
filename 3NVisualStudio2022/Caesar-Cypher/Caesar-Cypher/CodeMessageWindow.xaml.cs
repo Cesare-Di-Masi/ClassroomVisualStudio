b178260c76657cd8d33bd8022a22668a4381b98b
cesare di masi <cesare.dimasi.stud@ispascalcomandini.it>
2025-04-10 10:55:23 +0200

    Caesar Cypher WPF

---
+++ 3NVisualStudio2022/Caesar-Cypher/Caesar-Cypher/CodeMessageWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Caesar_CypherLib;
+
+namespace Caesar_Cypher
+{
+    /// <summary>
+    /// Logica di interazione per CodeMessageWindow.xaml
+    /// </summary>
+    public partial class CodeMessageWindow : Window
+    {
+        public CodeMessageWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void removeText(object sender, EventArgs e)
+        {
+            if(sender is TextBox textBox)
+            {
+                if (textBox.Text == "Write here")
+                    textBox.Text = "";
+            }
+        }
+
+        public void giveText(object sender, EventArgs e)
+        {
+            if (sender is TextBox textBox)
+            {
+                if (textBox.Text == "")
+                    textBox.Text = "Write here";
+            }
+        }
+
+        private void CodeMessage(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if(txtWriteYourMessage.Text.Length == 0 || txtWriteYourKey.Text.Length != 0)
+                {
+                    throw Exception("Please insert a message and a key");
+                }
+
+            }catch(Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+                return;
+            }
+
+
+            string message = txtWriteYourMessage.Text;
+            char key = txtWriteYourKey.Text[0];
+
+            CypherCode cypherCode = new CypherCode(message,key);
+
+            string codedMessage = cypherCode.codeMessage();
+
+            lblCodedMessage.Content = "Coded message: " + codedMessage;
+
+        }
+    }
+}
---
+++ 3NVisualStudio2022/Caesar-Cypher/Caesar-CypherLib/CypherCode.cs
@@ -0,0 +1,31 @@
+﻿namespace Caesar_CypherLib
+{
+    public class CypherCode
+    {
+        private String _messageToCode;
+        private char _key;
+
+        public CypherCode(string messageToCode, char key)
+        {
+            _messageToCode = messageToCode;
+            _key = key;
+        }
+
+        public string codeMessage()
+        {
+            int key = (int)_key;
+
+            for (int i = 0; i < _messageToCode.Length; i++)
+            {
+                char letter = _messageToCode[i];
+                char codedLetter = (char)(letter + key);
+                _messageToCode = _messageToCode.Remove(i, 1).Insert(i, codedLetter.ToString());
+            }
+
+            return _messageToCode;
+
+        }
+
+
+    }
+}
